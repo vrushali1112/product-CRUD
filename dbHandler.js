dae3d75b20455c01720e147094a97aa9e5a7f6b3
vrushali1112 <vrushali.sonawane@wohlig.com>
2020-10-23 11:35:26 +0530

    server and db handling done

---
+++ dbHandler.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose');
+var chalk = require('chalk');
+var development = require('./config/appConfig')
+
+
+//require chalk module to give colors to console text
+var connected = chalk.bold.cyan;
+var error = chalk.bold.yellow;
+var disconnected = chalk.bold.red;
+var termination = chalk.bold.magenta;
+
+
+
+//export this function and imported by server.js
+mongoose.connect(development.DB,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+});
+
+mongoose.connection.on('connected', function(){
+    console.log(connected("Mongoose default connection is open to ", development.DB));
+});
+
+mongoose.connection.on('error', function(err){
+    console.log(error("Mongoose default connection has occured "+err+" error"));
+});
+
+mongoose.connection.on('disconnected', function(){
+    console.log(disconnected("Mongoose default connection is disconnected"));
+});
+
+process.on('SIGINT', function(){
+    mongoose.connection.close(function(){
+        console.log(termination("Mongoose default connection is disconnected due to application termination"));
+        process.exit(0)
+    });
+});
